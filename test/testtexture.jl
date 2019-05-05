ad6eb6f99690592b00710d4658eefa9e978e47e3
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2019-05-05 11:48:07 +0200

    added srgb format tests

--- test/testtexture.jl
+++ test/testtexture.jl
@@ -22,6 +22,15 @@ c = createtestcontext()
     @test tex_alpha.id != 0
     @test tex_alpha.id != tex.id
     @testnoglerror bind(tex_alpha)
+    
+    @testnoglerror tex_srgb = Texture(img, internalformat=GL_SRGB)
+    @test tex.id != 0    
+    @testnoglerror bind(tex)
+    
+    @testnoglerror tex_alpha = Texture(img_alpha, internalformat=GL_SRGB_ALPHA)
+    @test tex_alpha.id != 0
+    @test tex_alpha.id != tex.id
+    @testnoglerror bind(tex_alpha)
 end
 
 # clean up test context
