deb522c1decd991825424f5c9e8a91271d256fa3
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2019-05-05 13:39:02 +0200

    added test to define swizzlemask

--- test/testtexture.jl
+++ test/testtexture.jl
@@ -1,9 +1,10 @@
 module TestTexture
 using ModernGL
 using Test
-using GLAbstraction: Texture, bind
+using GLAbstraction: Texture, bind, texparameter
 using StaticArrays
 using FileIO
+using Images
 
 include("testutils.jl")
 
@@ -23,6 +24,7 @@ c = createtestcontext()
     @test tex_alpha.id != tex.id
     @testnoglerror bind(tex_alpha)
     
+    # test srgb textures
     @testnoglerror tex_srgb = Texture(img, internalformat=GL_SRGB)
     @test tex.id != 0    
     @testnoglerror bind(tex)
@@ -31,6 +33,14 @@ c = createtestcontext()
     @test tex_alpha.id != 0
     @test tex_alpha.id != tex.id
     @testnoglerror bind(tex_alpha)
+    
+    # test greyvalue rgba using swizzlemask
+    @testnoglerror tex_swizzle = Texture(red.(img))
+    @test tex_swizzle.id != 0
+    @testnoglerror texparameter(tex_swizzle, GL_TEXTURE_SWIZZLE_RGBA, 
+            GLenum[GL_RED, GL_RED, GL_RED, GL_RED])
+    @testnoglerror bind(tex_swizzle)
+    
 end
 
 # clean up test context
